01099fa1fc476cade1b448ce2015c64e5a3ef1fd
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-30 15:26:40 -0400

    Fix variable usage in template.js to what is typical for JS

--- 2019/javascript/template.js
+++ 2019/javascript/template.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const read_input = function(year, day) {
+const readInput = function(year, day) {
     let filename = 'input/' + year + '_' + day + '_input.txt'
     return fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'})
 }
@@ -8,30 +8,30 @@ const read_input = function(year, day) {
 const run = function() {
 
     // Read data
-    let input = read_input(2019, 'XX');
+    let input = readInput(2019, 'XX');
 
     // Process input here to something useful
     console.log(input.length)
 
     // Get results using `fuel` function with recursive off and on
-    const result_part1 = 'TODO'
-    const result_part2 = 'TODO'
+    const resultPart1 = 'TODO'
+    const resultPart2 = 'TODO'
 
-    return [result_part1, result_part2];
+    return [resultPart1, resultPart2];
 }
 
 
-const print_res = function(res) {
+const printRes = function(res) {
     console.log('Answer for part 1: ' + res[0]);
     console.log('Answer for part 2: ' + res[1]);
 }
 
 
-const run_and_time = function(run) {
+const runAndTime = function(run) {
     console.time('Run function run-time')
     const res = run();
-    print_res(res);
+    printRes(res);
     console.timeEnd('Run function run-time')
 }
 
-run_and_time(run);
+runAndTime(run);
