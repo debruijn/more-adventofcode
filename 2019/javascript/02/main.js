fe24f48f697671b1cf53bbb8fe7d85670515f75c
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-30 15:24:52 -0400

    Add JS solution to 2019/02

---
+++ 2019/javascript/02/main.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+const readInput = function(year, day) {
+    let filename = 'input/' + year + '_' + day + '_input.txt'
+    return fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'})
+}
+
+
+const getOutput = function(data, noun, verb) {
+    data[1] = noun;
+    data[2] = verb;
+
+    for (let i = 0; i < data.length; i+=4) {
+        if (data[i] === 99) {
+            return data[0]
+        } else if (data[i] === 1) {
+            data[data[i+3]] = data[data[i+1]] + data[data[i+2]];
+        } else if (data[i] === 2) {
+            data[data[i+3]] = data[data[i+1]] * data[data[i+2]];
+        } else {
+            console.log("Wrong value for current position: " + data[i] + " at " + i);
+        }
+    }
+    return data[0]
+}
+
+
+const completeGravityAssist = function(data) {
+    for (let i=0; i < Math.min(100, data.length); i++) {
+        for (let j = 0; j < Math.min(100, data.length); j++) {
+            if (getOutput(data.slice(), i, j) === 19690720) {
+                return 100 * i + j
+            }
+        }
+    }
+}
+
+
+const run = function() {
+
+    // Read data
+    let input = readInput(2019, '02');
+
+    // Process input here to something useful
+    input = input.split(',')
+        .map(x => parseInt(x));
+
+    // Get results utility functions
+    const result_part1 = getOutput(input.slice(), 12, 2);
+    const result_part2 = completeGravityAssist(input);
+
+    return [result_part1, result_part2];
+}
+
+
+const printRes = function(res) {
+    console.log('Answer for part 1: ' + res[0]);
+    console.log('Answer for part 2: ' + res[1]);
+}
+
+
+const runAndTime = function(run) {
+    console.time('Run function run-time')
+    const res = run();
+    printRes(res);
+    console.timeEnd('Run function run-time')
+}
+
+runAndTime(run);
