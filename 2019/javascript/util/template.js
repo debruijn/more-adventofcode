058b0e096afb8dcdd5f4730bffbc22d93dc337f1
Bert de Bruijn <lpdebruijn@gmail.com>
2025-02-06 17:11:25 -0400

    Switch template script around to make use of util folder

---
+++ 2019/javascript/util/template.js
@@ -0,0 +1,18 @@
+import { readInput, runAndTime } from "../util/util.js";
+
+const run = function() {
+
+    // Read data
+    let input = readInput(2019, 'XX');
+
+    // Process input here to something useful
+    console.log(input.length)
+
+    // Get results
+    const resultPart1 = 'TODO'
+    const resultPart2 = 'TODO'
+
+    return [resultPart1, resultPart2];
+}
+
+runAndTime(run);
